4383d75f14b4dfdbdb16758afea3cd86c95cfad2
argoFuckYourself <55705328+argoFuckYourself@users.noreply.github.com>
2019-11-11 17:59:12 +0100

    Add files via upload

--- tests/driver_servo/main.c
+++ tests/driver_servo/main.c
@@ -27,10 +27,8 @@
 
 static servo_t servo;
 
-double d = 5; //Durchmesser der Spindel in cm
-
-
-//static xtimer_t timer;
+int d = 50; //Durchmesser der Spindel in mm
+int U;
 
 /*static int _alternativesAnsteuern(int argc, char**argv)
 {
@@ -92,9 +90,74 @@ static int _geschw(int argc, char**argv)
 
 
 
+static int _geschw_neu(int argc, char**argv)
+{
+	if (argc != 2) 
+	{
+        	printf("usage: %s <value> [mm] \n", argv[0]);
+	        return 1;
+    	}
+	
+	//VORBERECHNUNG
+
+	int l_ges = atoi(argv[1]);
+	int t_anfahrt = 2000; //Zeit zum Anfahren in ms
+	int t_step = (int)(t_anfahrt/7); //Zeit die für jeden Anfahr-Schritt benötigt wird in ms
+	//l_anfahrt = SUMME(f_step*t_step*U) = SUMME(f_step)*t_step*U
+	//SUMME(f_step)= 10/50200ms + 10/28600ms + 10/19600ms + 10/14900ms + 10/14100ms + 10/13300ms + 10/13100ms + 10/13000ms = 0.004723888
+	int l_anfahrt = (int)(0.004723888*t_step*U); //Länge, die bei der Anfahrt zurückgelegt wird in mm
+	int l_mitte = (int)(l_ges - l_anfahrt*2); //Länge, die während der konstanten Periode zurückgelegt wird in mm
+	int t_mitte = l_mitte / (0.000769231*U); // Zeit, die während der konstanten Periode vergeht in ms
+
+	//ÜBERPRÜFUNG	
+
+	/*printf("d = %d mm\n",d);
+	printf("U = %d mm\n",U);
+	printf("l_ges = %d mm\n",l_ges);
+	printf("t_anfahrt = %d ms\n",t_anfahrt);
+	printf("t_step = %d ms\n",t_step);
+	printf("t_mitte = %d ms\n",t_mitte);
+	printf("l_anfahrt = %d mm\n",l_anfahrt);
+	printf("l_mitte = %d mm\n",l_mitte);*/
+
+	//AUSFÜHRUNG
+	//Der Motor wird in 7 50-er Schritten auf die Maximalgeschwindigkeit gesetzt, solange, bis die gewünschte Länge eingeholt ist.
+	int dc=1500;
+	unsigned long int zeit;
+
+	//ANFAHREN
+	printf("<ANFAHREN> [%d ms]\n",t_anfahrt);
+	for (dc = 1500; dc>=1100;dc-=50)
+	{
+		servo_set(&servo, dc);
+		zeit=xtimer_now_usec(); //aktuelle Zeit wird gespeichert
+		while (xtimer_now_usec() < (zeit+t_step*1000));
+		//printf("%d\n",dc);
+	}
+
+	//KONST. GESCHWINDIGKEIT
+	printf("<MAXIMALGESCHWINDIGKEIT> [%d ms]\n",t_mitte);
+	zeit=xtimer_now_usec(); //aktuelle Zeit wird gespeichert
+	while (xtimer_now_usec() < (zeit+t_mitte*1000));
+
+	//BREMSEN
+	printf("<BREMSEN> [%d ms]\n",t_anfahrt);
+	for (dc = 1100; dc<=1500;dc+=50)
+	{
+		servo_set(&servo, dc);
+		zeit=xtimer_now_usec(); //aktuelle Zeit wird gespeichert
+		while (xtimer_now_usec() < (zeit+t_step*1000));
+		//printf("%d \n",dc);
+	}	
+	return 0;
+
+}
+
+
 static const shell_command_t shell_commands[] = { //Liste der Shell-Befehle
     { "set", "set servo value", _set },
     { "v" , "Geschwindigkeit (+/-) eingeben", _geschw},
+    {"test", "Länge einholen", _geschw_neu},
     {"kalibrieren" , "Stellt den Motor auf einen Standartwert, der mithilfe des Potis als Nullpunkt eingestellt werden muss.", _kalibrieren},
     { NULL, NULL, NULL }
 };
@@ -107,7 +170,7 @@ static const shell_command_t shell_commands[] = { //Liste der Shell-Befehle
 int main(void)
 {
     int res;
-
+    U =(int)(d*3.14159265358979323846); //Umfang der Spindel in mm
 
     puts("\nMotorsteuerung des RIOT-Projekts.");
     puts("Verbinden Sie den PWM-Eingang des Motors mit dem sechsten Pin von unten auf der rechten Seite");
