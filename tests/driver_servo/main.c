0189ef64ee76897edcb6ca102ebd4c44dd232029
argoFuckYourself <55705328+argoFuckYourself@users.noreply.github.com>
2019-11-18 14:39:38 +0100

    Add files via upload

--- tests/driver_servo/main.c
+++ tests/driver_servo/main.c
@@ -30,14 +30,7 @@ static servo_t servo;
 int d = 50; //Durchmesser der Spindel in mm
 int U;
 
-/*static int _alternativesAnsteuern(int argc, char**argv)
-{
-    if (argc != 2) {
-        printf("usage: %s <value> \n", argv[0]);
-        return 1;
-    }
-		
-}*/
+
 
 static int _kalibrieren(int argc, char**argv)
 {
@@ -51,6 +44,10 @@ static int _kalibrieren(int argc, char**argv)
 	printf("Stellen Sie mithlfe des Potis den Motor auf Stillstand\n");
 	return 0;
 }
+
+
+
+
 
 
 static int _set(int argc, char**argv)
@@ -68,46 +65,49 @@ static int _set(int argc, char**argv)
     return 0;
 }
 
-static int _geschw(int argc, char**argv)
-{
-	//int schritte= STILLSTAND-MAXGESCHW;
-	if (argc != 2) {
-		printf("Geben Sie %s und einen Wert zwischen -200 und +200 ein", argv[0]);	
-		return 1;
-	}
-	
-	unsigned v = atoi(argv[1]);		
 
-	//if (v > 20 || v < -10) {
-	//	printf("Geben Sie einen Wert zwischen -10 und 20 ein");
-	//	return 1;
-	//}
-	servo_set(&servo, STILLSTAND-v);
-	printf("set %u\n",STILLSTAND-v);
-	return 0;
-}
+
+
 
 
 
 
 static int _geschw_neu(int argc, char**argv)
 {
-	if (argc != 2) 
+	if (argc != 3) 
 	{
-        	printf("usage: %s <value> [mm] \n", argv[0]);
+        	printf("usage: %s <value> <unit> \n", argv[0]);
 	        return 1;
     	}
+
+	int l_ges = atoi(argv[1]); //Einlesen der Länge
+	
+	if 	(strcmp(argv[2],"mm")==0) ;
+	else if (strcmp(argv[2],"cm")==0) l_ges=l_ges*10;
+	else if (strcmp(argv[2],"m")==0) l_ges=l_ges*1000;
+	else{
+		 printf("Ungueltige Einheit\n");
+		return 1;
+	}	
 	
 	//VORBERECHNUNG
 
-	int l_ges = atoi(argv[1]);
+
 	int t_anfahrt = 2000; //Zeit zum Anfahren in ms
-	int t_step = (int)(t_anfahrt/7); //Zeit die für jeden Anfahr-Schritt benötigt wird in ms
+	int t_mitte = -1; // Zeit, die während der konstanten Periode vergeht in ms
+	int t_step; //Zeit die für jeden Anfahr-Schritt benötigt wird in ms
+	int l_anfahrt; //Länge, die bei der Anfahrt zurückgelegt wird in mm
+	int l_mitte; //Länge, die während der konstanten Periode zurückgelegt wird in mm
+	while (t_mitte<0){
+	t_step = (int)(t_anfahrt/7); 
 	//l_anfahrt = SUMME(f_step*t_step*U) = SUMME(f_step)*t_step*U
 	//SUMME(f_step)= 10/50200ms + 10/28600ms + 10/19600ms + 10/14900ms + 10/14100ms + 10/13300ms + 10/13100ms + 10/13000ms = 0.004723888
-	int l_anfahrt = (int)(0.004723888*t_step*U); //Länge, die bei der Anfahrt zurückgelegt wird in mm
-	int l_mitte = (int)(l_ges - l_anfahrt*2); //Länge, die während der konstanten Periode zurückgelegt wird in mm
-	int t_mitte = l_mitte / (0.000769231*U); // Zeit, die während der konstanten Periode vergeht in ms
+	l_anfahrt = (int)(0.004723888*t_step*U); 
+	l_mitte = (int)(l_ges - l_anfahrt*2); 
+	t_mitte = l_mitte / (0.000769231*U); 
+	if (t_mitte < 0) t_anfahrt -= 100;
+	}
+
 
 	//ÜBERPRÜFUNG	
 
@@ -118,7 +118,7 @@ static int _geschw_neu(int argc, char**argv)
 	printf("t_step = %d ms\n",t_step);
 	printf("t_mitte = %d ms\n",t_mitte);
 	printf("l_anfahrt = %d mm\n",l_anfahrt);
-	printf("l_mitte = %d mm\n",l_mitte);*/
+	printf("l_mitte = %d mm\n",l_mitte); */
 
 	//AUSFÜHRUNG
 	//Der Motor wird in 7 50-er Schritten auf die Maximalgeschwindigkeit gesetzt, solange, bis die gewünschte Länge eingeholt ist.
@@ -156,8 +156,7 @@ static int _geschw_neu(int argc, char**argv)
 
 static const shell_command_t shell_commands[] = { //Liste der Shell-Befehle
     { "set", "set servo value", _set },
-    { "v" , "Geschwindigkeit (+/-) eingeben", _geschw},
-    {"test", "Länge einholen", _geschw_neu},
+    {"+", "Länge einholen", _geschw_neu},
     {"kalibrieren" , "Stellt den Motor auf einen Standartwert, der mithilfe des Potis als Nullpunkt eingestellt werden muss.", _kalibrieren},
     { NULL, NULL, NULL }
 };
@@ -172,15 +171,15 @@ int main(void)
     int res;
     U =(int)(d*3.14159265358979323846); //Umfang der Spindel in mm
 
-    puts("\nMotorsteuerung des RIOT-Projekts.");
-    puts("Verbinden Sie den PWM-Eingang des Motors mit dem sechsten Pin von unten auf der rechten Seite");
+    /*puts("\nMotorsteuerung des RIOT-Projekts.");
+    puts("Verbinden Sie den PWM-Eingang des Motors mit dem sechsten Pin von unten auf der rechten Seite");*/
 
     res = servo_init(&servo, DEV, CHANNEL, SERVO_MIN, SERVO_MAX);
     if (res < 0) {
         puts("Errors while initializing servo");
         return -1;
     }
-    puts("Servo initialized.");
+    //puts("Servo initialized.");
 
     char line_buf[SHELL_DEFAULT_BUFSIZE];
     shell_run(shell_commands, line_buf, SHELL_DEFAULT_BUFSIZE);
